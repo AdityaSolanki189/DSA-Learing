4d13055b7582ebb25a9fefcd2150f5d079ee910c
Aditya <aditya2ss283@gmail.com>
2020-04-10 18:34:43 +0530

    FIRST commit

--- carfueling.c
+++ carfueling.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 
+
 int main(){
-    int d,m,n,cur=0,min=0,last=0;
+    int d,m,n,s[300],num_ref;
     scanf("%d%d%d",&d,&m,&n);
-    int stops[100000];
+     
     for(int i=0;i<n;i++){
-        scanf("%d",&stops[i]);
+        scanf("%d",&s[i]);
     }
     
-    while(cur<(d-1)){
-        last = cur;
-        while((cur<(d-1)) && (stops[cur+1] - stops[last]) <= m){
+    
+    num_ref = car_fuel(d,m,n,s);
+    printf("%d",num_ref);
+    return 0;
+}
+
+int car_fuel(int d,int m,int n,int s[]){
+    int cur=0,min=0;
+    while(cur<=d){
+        
+        int last =cur;
+        while(cur<=d && (s[cur+1] - s[last])<=m){
             cur++;
-            if(cur == (d-1))
-                break;
         }
-        if(cur ==last){
-            min=-1;
+        if(cur==last){
+            return -1;
             break;
         }
-        if(cur < (d-1)){
+        if(cur<=d){
             min++;
         }
-
     }
-    printf("%d",min);
+    return min;
 }
+
