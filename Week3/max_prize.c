ae0441c784d87faf482d2044b71f715cbd111b02
Aditya <aditya2ss283@gmail.com>
2020-04-11 05:38:15 +0530

    new commit

--- Week3/max_prize.c
+++ Week3/max_prize.c
@@ -3,26 +3,23 @@
 int main(){
     int n,count=0;
     scanf("%d",&n);
-    int a[1000]={0};
-    for (int i = 1; i<=n; i++) {
-        if (n - i >= i) {
-            
-            a[i]=i;
+    int a[100000]={0};
+    for(int i=1;i<=n;i++){
+        if(n-i > i){
+            a[i] = i;
             count++;
             n= n-i;
-        } 
-        else {
-            a[i] = n;
+        }
+        else{
+            a[i] += n;
             count++;
             break;
         }
-    
-    }   
+    }  
     printf("%d\n",count);
     for(int i=1;i<=n;i++){
-        if(a[i]>0){
-        printf("%d ",a[i]);
-        }
+        if(a[i]>0)
+            printf("%d ",a[i]);
     }
     return 0;
 }
