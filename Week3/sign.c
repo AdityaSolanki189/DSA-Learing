5acd0c718ea6d8443e8174dc10cf644b66590dca
Aditya <aditya2ss283@gmail.com>
2020-04-13 02:48:25 +0530

    new

--- Week3/sign.c
+++ Week3/sign.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
-
-int main(){
-    int num,i,j;
-    scanf("%d",num);
-    long int a[100],b[100],temp=0;
-    for(i=0;i<num;i++){
-        scanf("%ld%ld",&a[i],&b[i]);
-    }
+void sort(long int s[],long int f[],int num){
+    int i=0,j=0;
     for(i=0;i<num;i++){                //sorting in ascending
         for(j=i+1;j<num;j++){
-            if(a[i]>a[j]){
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-
-                temp = b[i];
-                b[i] = b[j];
-                b[j] = temp;
+            if(f[i]>f[j]){
+                long int temp = f[i];
+                f[i] = f[j];
+                f[j] = temp;
+                temp=0;
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
             }
         }
     }
+}
+void min_interval(long int s[],long int f[],int num){
     
-    int com,count=0,last;
+}
+int main(){
+    int num,i,j;
+    scanf("%d",&num);
+    long int s[100],f[100],temp=0;
     for(i=0;i<num;i++){
-        
+        scanf("%ld%ld",&s[i],&f[i]);
     }
-
-
+    sort(s,f,num);
+    
+    return 0;
 }
