45f6730f81ee799e5d3dc6f3d76c323d5e1fad30
Aditya <aditya2ss283@gmail.com>
2020-04-13 06:47:08 +0530

    new

---
+++ Week2/fibo_last.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fibo_n(int fibo[],int num){
+    fibo[0]=0;
+    fibo[1]=1;
+    for(int i=2;i<=num;i++){
+        fibo[i] = (fibo[i-1] + fibo[i-2])%10;
+    }
+    return fibo[num];
+}
+int fibo[1000000];
+int main(){
+    int num;
+    scanf("%d",&num);
+    
+    printf("%d",fibo_n(fibo,num));
+    return 0;
+}
